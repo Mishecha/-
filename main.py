8afca0b3dcd20a7795a732c963f619b104a81bf4
Mishecha <85157304+Mishecha@users.noreply.github.com>
2022-07-14 19:16:52 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,40 @@
-import asyncio
 import os.path
 import os
-import random
+from os import listdir
 import time
+import random
 
 import telegram
 from dotenv import load_dotenv
 
-nasa_epic_image_directory = os.environ['nasa_epic_image_directory']
-nasa_image_directory = os.environ['nasa_image_directory']
-spacex_image_directory = os.environ['spacex_image_directory']
-telegram_token = os.environ['TELEGRAM_TOKEN']  
-telegram_chat_id = os.environ['TELEGRAM_CHAT_ID']
-delay = int(os.environ['DELAY'])
-bot = telegram.Bot(telegram_token)
 
-def get_file_path(names_directory):
-    name_directory = random.choice(names_directory)
-    name_image = random.choice(os.listdir(name_directory))
-    file_path = f'{name_directory}/{name_image}'
-    return file_path
-
-
-async def send_photo(file_path):
-    with open(file_path, 'rb') as file:
-        bot.send_photo(chat_id=telegram_chat_id, photo=file)
-
+def get_random_path(names_directory):
+    random_folder = random.choice(names_directory)
+    random_file = random.choice(listdir(random_folder))
+    random_path = f'{random_folder}/{random_file}'
+    return random_path
+    
 
 if __name__ == "__main__":
     load_dotenv()
-  
-    names_directory = [
-                    nasa_epic_image_directory, 
-                    spacex_image_directory, 
-                    nasa_image_directory
-    ]
 
-    file_path = get_file_path(names_directory)
-  
+    nasa_epic_image_directory = os.environ['nasa_epic_image_directory']
+    nasa_image_directory = os.environ['nasa_image_directory']
+    spacex_image_directory = os.environ['spacex_image_directory']
+    telegram_token = os.environ['TELEGRAM_TOKEN']  
+    telegram_chat_id = os.environ['TELEGRAM_CHAT_ID']
+    delay = int(os.environ['DELAY'])
+    
+    bot = telegram.Bot(telegram_token)
+
+    names_directory = [
+        nasa_epic_image_directory,
+        spacex_image_directory,
+        nasa_image_directory
+    ]
+    
     while True:
-        asyncio.run(send_photo(file_path))
+        file_path = get_random_path(names_directory)
+        with open(file_path, 'rb') as file:
+            bot.send_photo(chat_id=telegram_chat_id, photo=file)
         time.sleep(delay)
