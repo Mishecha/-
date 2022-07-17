7adf9676a4d2f9959016ba02e31ccc6fac58cb2e
Mishecha <85157304+Mishecha@users.noreply.github.com>
2022-07-17 19:37:57 +1100

    Update download_photos_spacex.py

--- download_photos_spacex.py
+++ download_photos_spacex.py
@@ -1,36 +1,14 @@
-import urllib.parse
 import os.path
 import os
 from pathlib import Path
-from urllib.parse import urlparse
 
 import requests
 from dotenv import load_dotenv
 
+from download_image import download_image
 
-
-def download_image(url, file_path, params1=''):
-    response = requests.get(url, params=params1)
-    response.raise_for_status()
-    with open(file_path, 'wb') as file:
-        file.write(response.content)
-
-
-def create_folders_for_pictures():
-    space_pictures_folders = ['image_spacex']
-    for folder in space_pictures_folders:
-        Path(folder).mkdir(parents=True, exist_ok=True)
-
-
-def get_extension(user_link):
-    user_unquote_link = urllib.parse.unquote(user_link, encoding='utf-8', errors='replace')
-    parsed_link = urlparse(user_unquote_link)
-    path = parsed_link.path
-    splitext = os.path.splitext(path)
-    return splitext[1]
   
-def download_photos_spacex(spacex_image_directory):
-    spacex_last_launch = 66
+def download_photos_spacex(spacex_image_directory, spacex_last_launch=66):
     spacex_file_name = 'image_spacex'
     spacex_url = 'https://api.spacexdata.com/v4/launches'
 
@@ -45,13 +23,11 @@ def download_photos_spacex(spacex_image_directory):
 
 
 def main():
-    load_dotenv()    
-    spacex_image_directory = os.environ['spacex_image_directory']
-  
-    create_folders_for_pictures()
+    load_dotenv()
+    Path('image_spacex').mkdir(parents=True, exist_ok=True)
+    spacex_image_directory = os.environ['SPACEX_IMAGE_DIRECTORY']
+
     download_photos_spacex(spacex_image_directory)
 
 if __name__ == "__main__":
     main()
-
-
