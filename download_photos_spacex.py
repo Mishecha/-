05bc85cceb01ddb40dbf12aa7f0e2f2794b51147
Mishecha <85157304+Mishecha@users.noreply.github.com>
2022-07-31 19:54:42 +1100

    убрал длинные строки

--- download_photos_spacex.py
+++ download_photos_spacex.py
@@ -19,7 +19,8 @@ def download_photos_spacex(spacex_image_directory, spacex_last_launch=66):
     )[spacex_last_launch]['links']['flickr']['original']
 
     for number, image in enumerate(spacex_link):
-        download_image(image, f'{spacex_image_directory}/{number}{spacex_file_name}.jpg')
+        download_image(image, f'{spacex_image_directory}/{number}'
+                       f'{spacex_file_name}.jpg')
 
 
 def main():
