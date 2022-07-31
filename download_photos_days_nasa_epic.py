588e273af8bca9956358313a14021ee73245a693
Mishecha <85157304+Mishecha@users.noreply.github.com>
2022-07-31 19:23:43 +1100

    убрал длинные строки

--- download_photos_days_nasa_epic.py
+++ download_photos_days_nasa_epic.py
@@ -9,7 +9,8 @@ from dotenv import load_dotenv
 from download_image import download_image
 
 
-def download_photos_nasa_epic(nasa_api_key, nasa_epic_image_directory, number_images=9):
+def download_photos_nasa_epic(nasa_api_key,
+                              nasa_epic_image_directory, number_images=9):
     nasa_epic_params = {'api_key': nasa_api_key}
     nasa_epic_file_name = 'image_nasa_epic.png'
     nasa_epic_url = 'https://api.nasa.gov/EPIC/api/natural/images'
@@ -20,12 +21,15 @@ def download_photos_nasa_epic(nasa_api_key, nasa_epic_image_directory, number_im
         nasa_epic_image = image['image']
         nasa_epic_date = image['date']
 
-        time_date = datetime.datetime.strptime(nasa_epic_date, '%Y-%m-%d  %H:%M:%S')
+        time_date = datetime.datetime.strptime(nasa_epic_date,
+                                               '%Y-%m-%d  %H:%M:%S')
         format_date = time_date.strftime('%Y/%m/%d')
 
-        nasa_epic_url = f'https://api.nasa.gov/EPIC/archive/natural/{format_date}/png/{nasa_epic_image}.png'
+        nasa_epic_url = (f'https://api.nasa.gov/EPIC/archive/natural/'
+                         f'{format_date}/png/{nasa_epic_image}.png')
 
-        download_image(nasa_epic_url, f'{nasa_epic_image_directory}/{number}{nasa_epic_file_name}', nasa_epic_params)
+        download_image(nasa_epic_url, f'{nasa_epic_image_directory}/{number}\
+                      {nasa_epic_file_name}', nasa_epic_params)
 
 
 def main():
