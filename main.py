7bf25c7f63bcc066973899c2b9d90e2bb04fc8b6
Mishecha <85157304+Mishecha@users.noreply.github.com>
2022-08-04 19:45:13 +1100

    сократил код

--- main.py
+++ main.py
@@ -8,34 +8,25 @@ import telegram
 from dotenv import load_dotenv
 
 
-def get_random_path(names_directory):
-    random_folder = random.choice(names_directory)
-    random_file = random.choice(listdir(random_folder))
-    random_path = f'{random_folder}/{random_file}'
+def get_random_path(name_directory):
+    random_file = random.choice(listdir(name_directory))
+    random_path = f'{name_directory}/{random_file}'
     return random_path
     
 
 if __name__ == "__main__":
     load_dotenv()
 
-    nasa_epic_image_directory = os.environ['SPACE_IMAGE_DIRECTORY']
-    nasa_image_directory = os.environ['SPACE_IMAGE_DIRECTORY']
-    spacex_image_directory = os.environ['SPACE_IMAGE_DIRECTORY']
+    space_image_directory = os.environ['SPACE_IMAGE_DIRECTORY']
     telegram_token = os.environ['TELEGRAM_TOKEN']  
     telegram_chat_id = os.environ['TELEGRAM_CHAT_ID']
     delay = int(os.environ['DELAY'])
     
     bot = telegram.Bot(telegram_token)
 
-    names_directory = [
-        nasa_epic_image_directory,
-        spacex_image_directory,
-        nasa_image_directory
-    ]
-
     try:
         while True:
-            file_path = get_random_path(names_directory)
+            file_path = get_random_path(space_image_directory)
             with open(file_path, 'rb') as file:
                 bot.send_photo(chat_id=telegram_chat_id, photo=file)
             time.sleep(delay)
