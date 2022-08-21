97cf64ae8df2d91a38581fc9c2d2f06a7f6430d4
Mishecha <85157304+Mishecha@users.noreply.github.com>
2022-08-21 19:48:06 +1100

    сделал значение по дефолту

--- download_photos_days_nasa.py
+++ download_photos_days_nasa.py
@@ -41,7 +41,7 @@ def download_photos_days_nasa(nasa_api_key,
 
 def main():
     load_dotenv()
-    space_image_directory = 'space_image'
+    space_image_directory = os.getenv('SPACE_IMAGE_DIRECTORY', default='space_image')
     Path(space_image_directory).mkdir(parents=True, exist_ok=True)
     nasa_api_key = os.environ['NASA_API_KEY']
 
